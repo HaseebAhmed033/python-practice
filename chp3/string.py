0e892fe07fbaaa4b61d18429983b7262bd018e03
Haseeb Ahmed <haseebahmed33@protonmail.com>
2026-04-05 09:02:17 +0500

    working on python strings

---
+++ chp3/string.py
@@ -0,0 +1,38 @@
+# a="Haseeb"
+# b='Ahmed'
+# c='''is my name'''
+# d='''
+# this is 
+# a
+# very long 
+# string 
+# text and thats why we use triple quotes here'''
+
+# print(a,'',b,' ',c,' ',d)
+# # String Concatenation
+# e=a+b+c+d   
+# print(e)
+# # String Repetition
+# f=a*3               
+# print(f)
+
+name ="Haseeb" 
+
+# nick=name[0:4] #will go from 0 to 1,2,2 NOT 4 bcz 4 is NOT INCLUDED
+# print(nick)
+
+# #negative indexing ------ b=-1,e=-2,e=-3....H=-6
+# nick2=name[-5:] 
+# print(nick2)
+
+
+# print(len(name)) #length of the string
+# print(type(name)) #type of the variable
+
+# t=int(len(name)) #typecasting the length to integer
+# print(t,type(t))
+
+print(name[0:]) 
+
+alp="abcdefghijklmnopqrstuvwxyz"
+print(alp[0:26:4]) #will print every 4th character by first go 0 to 25 then jump 4,4 gap
