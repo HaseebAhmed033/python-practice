0c756dd763cea76f2bfaf2681e6645b93a20a35c
Haseeb Ahmed <haseebahmed33@protonmail.com>
2026-04-12 07:55:44 +0500

    chp4 done

---
+++ chp4/list.py
@@ -0,0 +1,21 @@
+# list1= ["apple",2,3.43,True]
+
+# #string does not change but original list can be changed
+
+# list1.append("banana")
+
+# print(list1)
+
+
+
+# list1.insert(999,"grapes ")
+# print(list1)
+
+
+
+l2= [1,2,3,4,5]
+# l2.pop(2)
+# print(l2)
+
+a=sum(l2)
+print(a)
---
+++ chp4/tuple.py
@@ -0,0 +1,23 @@
+# t=(1,"Ali",2.77, False,2.77)
+
+# print(t.count(2.77))
+
+
+
+# # print(t)
+
+# a=t.index(2.77)
+# print(a)
+
+
+# t[2]=2.3
+ #cant change tuple
+
+# print(t)
+
+
+t=(0,0,0,0,0,2,22,2,)
+
+a=print(t.count(0))
+
+print(a)
